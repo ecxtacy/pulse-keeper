eca11c469dfdce11ac27554ece118f7dae1ba261
ecxtacy <dc.dhruvchouhan@gmail.com>
2024-04-10 12:56:29 +0530

    feat(server): initialize a basic express server
    Packages - nodemon(for development), typescript, express,
    ts-node for compilation.

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send("pulse-keeper server is up !!!");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
