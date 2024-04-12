a243dab7113ff68d49614f870478431c24b9243e
ecxtacy <dc.dhruvchouhan@gmail.com>
2024-04-12 10:35:26 +0530

    feat(middleware): add essential middlewares
    Following middlewares are added.
    1. helmet - security headers.
    2. cors - cross origin requests.
    3. morgan - logging.
    4. json - parsing json body.
    5. compression - gzip compression for increased performance.
    
    Fixes #2.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,18 @@
 import express from "express";
 import v1Router from "./routes/v1.router";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import compression from "compression";
 
 const app = express();
+
+// Add essential middlewares
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(compression());
 
 app.get("/api/v1", v1Router);
 
